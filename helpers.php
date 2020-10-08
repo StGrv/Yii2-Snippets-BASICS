0d4d7e0a17e3ff3fcfd17f414eef24bf8c0a6823
StGrv <65493257+StGrv@users.noreply.github.com>
2020-10-08 12:09:31 +0300

    HTML Helpers methods

---
+++ helpers.php
@@ -0,0 +1,28 @@
+<?php // HTML Helpers - Tags ?>
+
+<?= Html::tag('h2', 'Humen must explore h2', ['class' => 'post-title', 'style' => 'font-size:30px; color:red']) ?>
+
+<?= Html::tag('h3', 'Humen must explore h3', ['class' => 'post-subtitle']) ?>
+
+<?=Html::tag('p', 'Humen must explore p', ['class' => 'post-meta'] ) ?>
+
+<?php // HTML Helpers - ActiveForm ?>
+
+<?php $form = ActiveForm::begin([
+    'options' => ['enctype' => 'multipart/form-data']
+]) ?>
+
+    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+    <?= $form->field($avatar, 'image[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
+    <?= $form->field($model, 'subject')->dropDownList('first', 'second', 'third') ?>
+
+<?php ActiveForm::end() ?>
+
+<?php // HTML Helpers - Img/Link ?>
+
+<?=Html::img(Url::to('@web/images/test.jpg'), ['class' => 'img-responsive']) ?>
+
+<img src="<?=Url::to('@web/img/test.jpg')?>" tag="Image" class="myclass">
+
+<?=Html::a('This is the title', ['/site/about']) ?>
