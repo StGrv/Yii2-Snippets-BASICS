633fe9d13f56964f8811438675e4793b9ff8dbc5
StGrv <65493257+StGrv@users.noreply.github.com>
2020-10-08 12:11:05 +0300

    HTML Helpers Methods

--- helpers.php
+++ helpers.php
@@ -1,10 +1,10 @@
 <?php // HTML Helpers - Tags ?>
 
-<?= Html::tag('h2', 'Humen must explore h2', ['class' => 'post-title', 'style' => 'font-size:30px; color:red']) ?>
+<?= Html::tag('h2', 'People must explore h2', ['class' => 'post-title', 'style' => 'font-size:30px; color:red']) ?>
 
-<?= Html::tag('h3', 'Humen must explore h3', ['class' => 'post-subtitle']) ?>
+<?= Html::tag('h3', 'People must explore h3', ['class' => 'post-subtitle']) ?>
 
-<?=Html::tag('p', 'Humen must explore p', ['class' => 'post-meta'] ) ?>
+<?=Html::tag('p', 'People must explore p', ['class' => 'post-meta'] ) ?>
 
 <?php // HTML Helpers - ActiveForm ?>
 
