b0825c3ee98774207a9d55763633ddc19297351b
StGrv <65493257+StGrv@users.noreply.github.com>
2022-06-09 15:07:11 +0300

    Update radioList map.php

--- radioList map.php
+++ radioList map.php
@@ -3,7 +3,7 @@
             'item'     => function ( $index, $label, $name, $checked, $value ) {
                 $return = '<label>';
                 $return .= '<input type="radio" name="' . $name . '" value="' . $value . '" ' . ( $checked == 1 ? 'checked' : NULL ) . ' >';
-                $return .= '<span style="margin-left: 5px;">' . ucwords( $label ) . '</span>';
+                $return .= '<span>' . ucwords( $label ) . '</span>';
                 $return .= '</label>';
                 return $return;
             },
